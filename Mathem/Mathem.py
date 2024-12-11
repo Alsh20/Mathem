5d6abbecdc17a9f0e72667f7a9558e211897600e
Alsh20 <ali23042008@gmail.com>
2024-12-11 15:34:49 +0500

    Update Mathem.py

--- Mathem/Mathem.py
+++ Mathem/Mathem.py
@@ -1,5 +1,4 @@
-﻿while True:
-    print()
+while True:
 #    user_input = input("Выберите 1) sin(alpha), 2) cos(alpha), 3) tg(alpha), 4) ctg(alpha) или 'exit' для выхода: ").strip()
 #    
 #    if user_input.lower() == 'exit':
@@ -16,92 +15,133 @@
     f = " ".join([d[0], d[1]])
     if x == 1 and f == "0 -":
         print("Жауабы: -sin(alpha)")
+        print()
     elif x == 1 and f == "0 +":
         print("Жауабы: sin(alpha)")
+        print()
     elif x == 1 and f == "90 -" or f == "pi/2 -":
         print("Жауабы: cos(alpha)")
+        print()
     elif x == 1 and f == "90 +" or f == "pi/2 +":
         print("Жауабы: cos(alpha)")
+        print()
     elif x == 1 and f == "180 -" or f == "pi -":
         print("Жауабы: sin(alpha)")
+        print()
     elif x == 1 and f == "180 +" or f == "pi +":
         print("Жауабы: -sin(alpha)")
+        print()
     elif x == 1 and f == "270 -" or f == "3pi/2 -":
         print("Жауабы: -cos(alpha)")
+        print()
     elif x == 1 and f == "270 +" or f == "3pi/2 +":
         print("Жауабы: -cos(alpha)")
+        print()
     elif x == 1 and f == "360 -" or f == "2pi -":
         print("Жауабы: -sin(alpha)")
+        print()
     elif x == 1 and f == "360 +" or f == "2pi +":
         print("Жауабы: sin(alpha)")
+        print()
 
     #cos
     elif x == 2 and f == "0 -":
         print("Жауабы: cos(alpha)")
+        print()
     elif x == 2 and f == "0 +":
         print("Жауабы: cos(alpha)")
+        print()
     elif x == 2 and f == "90 -" or f == "pi/2 -":
         print("Жауабы: sin(alpha)")
+        print()
     elif x == 2 and f == "90 +" or f == "pi/2 +":
         print("Жауабы: -sin(alpha)")
+        print()
     elif x == 2 and f == "180 -" or f == "pi -":
         print("Жауабы: -cos(alpha)")
+        print()
     elif x == 2 and f == "180 +" or f == "pi +":
         print("Жауабы: -cos(alpha)")
+        print()
     elif x == 2 and f == "270 -" or f == "3pi/2 -":
         print("Жауабы: -sin(alpha)")
+        print()
     elif x == 2 and f == "270 +" or f == "3pi/2 +":
         print("Жауабы: sin(alpha)")
+        print()
     elif x == 2 and f == "360 -" or f == "2pi -":
         print("Жауабы: cos(alpha)")
+        print()
     elif x == 2 and f == "360 +" or f == "2pi +":
         print("Жауабы: cos(alpha)")
+        print()
 
     #tg
     elif x == 3 and f == "0 -":
         print("Жауабы: -tg(alpha)")
+        print()
     elif x == 3 and f == "0 +":
         print("Жауабы: tg(alpha)")
+        print()
     elif x == 3 and f == "90 -" or f == "pi/2 -":
         print("Жауабы: ctg(alpha)")
+        print()
     elif x == 3 and f == "90 +" or f == "pi/2 +":
         print("Жауабы: -ctg(alpha)")
+        print()
     elif x == 3 and f == "180 -" or f == "pi -":
         print("Жауабы: -tg(alpha)")
+        print()
     elif x == 3 and f == "180 +" or f == "pi +":
         print("Жауабы: tg(alpha)")
+        print()
     elif x == 3 and f == "270 -" or f == "3pi/2 -":
         print("Жауабы: ctg(alpha)")
+        print()
     elif x == 3 and f == "270 +" or f == "3pi/2 +":
         print("Жауабы: -ctg(alpha)")
+        print()
     elif x == 3 and f == "360 -" or f == "2pi -":
         print("Жауабы: -tg(alpha)")
+        print()
     elif x == 3 and f == "360 +" or f == "2pi +":
         print("Жауабы: tg(alpha)")
+        print()
 
     #ctg
     elif x == 4 and f == "0 -":
         print("Жауабы: -ctg(alpha)")
+        print()
     elif x == 4 and f == "0 +":
         print("Жауабы: ctg(alpha)")
+        print()
     elif x == 4 and f == "90 -" or f == "pi/2 -":
         print("Жауабы: tg(alpha)")
+        print()
     elif x == 4 and f == "90 +" or f == "pi/2 +":
         print("Жауабы: -tg(alpha)")
+        print()
     elif x == 4 and f == "180 -" or f == "pi -":
         print("Жауабы: -ctg(alpha)")
+        print()
     elif x == 4 and f == "180 +" or f == "pi +":
         print("Жауабы: ctg(alpha)")
+        print()
     elif x == 4 and f == "270 -" or f == "3pi/2 -":
         print("Жауабы: tg(alpha)")
+        print()
     elif x == 4 and f == "270 +" or f == "3pi/2 +":
         print("Жауабы: -tg(alpha)")
+        print()
     elif x == 4 and f == "360 -" or f == "2pi -":
         print("Жауабы: -ctg(alpha)")
+        print()
     elif x == 4 and f == "360 +" or f == "2pi +":
         print("Жауабы: ctg(alpha)")
+        print()
     else:
         print("Неправильный синтаксис")
+        print()
 
 #    cont = input("Хотите продолжить? (да/нет): ").strip().lower()
 #    if cont != "да":
